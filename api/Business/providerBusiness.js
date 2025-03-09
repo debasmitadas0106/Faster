47bb940f29ab67cfbbe63a71a9230d14389af485
allusurendra-ai <allusurendra8@gmail.com>
2025-03-10 00:51:25 +0530

    patch is added

--- api/Business/providerBusiness.js
+++ api/Business/providerBusiness.js
@@ -1,62 +1,79 @@
 const {
-    findproviderService,
-    createproviderservice,
-    deleteproviderservice}=require("../Service/providerService");
+  findproviderService,
+  createproviderservice,
+  deleteproviderservice,
+  updateproviderservice,
+} = require("../Service/providerService");
 
-const providerBussiness=async (payload,query) =>{
-    try{
-        let{username,password,email}=payload;
-    const dbPayload={
-        ...payload,
+const createproviderBusiness = async (payload, query) => {
+  try {
+    let { username, password, email } = payload;
+    const dbpayload = {
+      ...payload,
     };
-    const getprovider=await findproviderService({
-        $or:[{username},{email}],
+    console.log("createbusinesspayload=", dbpayload);
+    const getprovider = await findproviderService({
+      $or: [{ username }, { email }],
     });
-    if(getprovider){
-        return getprovider.username===username
+    if (getprovider) {
+      return getprovider.username === username
         ? "Username already exists in provider"
         : "Email already exists in provider";
     }
-    const userDetails=await createproviderservice(dbPayload);
+    const userDetails = await createproviderservice(dbpayload);
     return userDetails;
-}
-catch(error){
+  } catch (error) {
     console.log(error);
-}
+  }
 };
 
-const getproviderBussiness=async (payload,query) => {
-    try{
-        let {username,password,email}=payload;
-        const dbPayload={
-            ...payload,
-        };
-        const getprovider=await findproviderService({
-            $or:[{username},{email}],
-        });
-        return getprovider;
+const getproviderBusiness = async (payload, query) => {
+  try {
+    let { username, password, email } = payload;
+    const dbPayload = {
+      ...payload,
+    };
+    const getprovider = await findproviderService({
+      $or: [{ username }, { email }],
+    });
+    return getprovider;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-    }
-    catch(error){
-        console.log(error);
-    }
-}
+const deleteproviderBusiness = async (payload, query) => {
+  try {
+    let { username, email } = payload;
+    const dbPayload = {
+      ...payload,
+    };
+    // const deleteprovider=await findproviderService({
+    //     $or:[(username),{email}],
+    // });
+    const providerdetails = await deleteproviderservice(dbPayload);
+    return providerdetails;
+  } catch (error) {
+    console.log(error);
+  }
+};
+const updateproviderBusiness = async (payload, query) => {
+  try {
+    console.log("payload==", payload);
+    let { username, email } = payload;
+    const dbpayload = {
+      ...payload,
+    };
+    const providerdetails = await updateproviderservice(dbpayload);
+    return providerdetails;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const deleteproviderBussiness=async (payload,query) => {
-    try{
-        let {username,email}=payload;
-        const dbPayload={
-            ...payload,
-        };
-        const deleteprovider=await findproviderService({
-            $or:[(username),{email}],
-        });
-        const providerdetails=await deleteproviderservice(dbPayload);
-        return providerdetails
-    }
-    catch(error){
-        console.log(error);
-    }
-}
-
-module.exports={providerBussiness,getproviderBussiness,deleteproviderBussiness};
+module.exports = {
+  createproviderBusiness,
+  getproviderBusiness,
+  deleteproviderBusiness,
+  updateproviderBusiness,
+};
--- api/Controller/providerController.js
+++ api/Controller/providerController.js
@@ -1,60 +1,84 @@
-const {providerBussiness,getproviderBussiness,deleteproviderBussiness}=require("../Business/providerBusiness")
+const {
+  createproviderBusiness,
+  getproviderBusiness,
+  deleteproviderBusiness,
+  updateproviderBusiness,
+} = require("../Business/providerBusiness");
 
-const createprovidercontroller=async (req,res) =>{
-    try{
-        const resp=await providerBussiness(req.body,req.query);
-        return res.status(200).json({
-            success:true,
-            data:resp,
-        });
-    }catch(error){
-        console.error("Error in create provider controller:",error);
-        return res.status(500).json({
-            success:false,
-            message: "Internal server error",
-            error:error.message,
-        });
-    }
+const createprovidercontroller = async (req, res) => {
+  try {
+    const resp = await createproviderBusiness(req.body, req.query);
+    return res.status(200).json({
+      success: true,
+      data: resp,
+    });
+  } catch (error) {
+    console.error("Error in create provider controller:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
 };
 
-const getprovidercontroller=async(req,res) => {
-    try{
-        console.log('req=',req.query,'res=',res.query);
-        const resp=await getproviderBussiness(req.query,res.query);
-        // console.log('resp=',resp)
-        return res.status(200).json({
-            success:true,
-            data:resp,
-        });
-    }
-    catch(error){
-        console.error("Error in get provider controller:",error);
-        return res.status(500).json({
-        success:false,
-        message: "Internal server error",
-        error:error.message,
+const getprovidercontroller = async (req, res) => {
+  try {
+    console.log("req=", req.query, "res=", res.query);
+    const resp = await getproviderBusiness(req.query, res.query);
+    // console.log('resp=',resp)
+    return res.status(200).json({
+      success: true,
+      data: resp,
     });
-    }
-}
+  } catch (error) {
+    console.error("Error in get provider controller:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
+};
 
-const deleteprovidercontroller=async(req,res) => {
-    try{
-        console.log('req=',req.query,'res=',res.body)
-        const resp=await deleteproviderBussiness(req.query,req.query);
-        console.log('resp==',resp);
-        return res.status(200).json({
-            success:true,
-            data:resp,
-        });
-    }
-    catch(error){
-        console.error("Error in delete provider controller",error);
-        return res.status(500).json({
-            success:false,
-            message:"Internal server error",
-            error:error.message,
-        })
-    }
-}
+const deleteprovidercontroller = async (req, res) => {
+  try {
+    const resp = await deleteproviderBusiness(req.query, req.query);
+    console.log("resp==", resp);
+    return res.status(200).json({
+      success: true,
+      data: resp,
+    });
+  } catch (error) {
+    console.error("Error in delete provider controller", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
+};
+const updateprovidercontroller = async (req, res) => {
+  try {
+    // console.log('controller is ','req=',req.body,'res=',res.body)
+    const resp = await updateproviderBusiness(req.body, res.body);
+    return res.status(200).json({
+      success: true,
+      data: resp,
+    });
+  } catch (error) {
+    console.error("Error in delete provider controller", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
+};
 
-module.exports={createprovidercontroller,getprovidercontroller,deleteprovidercontroller};
+module.exports = {
+  createprovidercontroller,
+  getprovidercontroller,
+  deleteprovidercontroller,
+  updateprovidercontroller,
+};
