216a8729c13f1ab4b7e97e74a12547b2849ac1d2
Debasmita Das <debasmita@vodex.ai>
2025-06-16 17:29:39 +0530

    db verifcation middleware

---
+++ middlewares/verifyDatabase.js
@@ -0,0 +1,33 @@
+const { findUserService } = require("../api/Service/userService");
+const { METHODS, STATUS } = require("../utils/constants");
+const Logger = require("../utils/logger");
+const { VERIFY_DB_BLACKLIST } = require("./constants");
+
+const verifyDb = async (req, res, next) => {
+  const logger = new Logger(
+    `${METHODS.ENTERING_TO} || ${METHODS.VERIFY_DATABASE}}`
+  );
+  if (VERIFY_DB_BLACKLIST.includes(req.url)) {
+    console.log("bypass database verification");
+    logger.debug("bypass database verification");
+    return next();
+  }
+  if (!req.user.db_name) {
+    return res.status(STATUS.SUCCESS).send({
+      status: STATUS.BAD_REQUEST,
+      errMsg: "account Url is required.",
+    });
+  }
+  let user = await findUserService(
+    { userId: req.user.userDetails._id },
+    req.user.db_name
+  );
+  if (user) {
+    return next();
+  } else {
+    return res.status(401).json({ msg: "User Unauthorized" });
+  }
+};
+module.exports = {
+  verifyDb,
+};
